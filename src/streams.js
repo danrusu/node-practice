4812d9f7889e161750760c85080b191dd105f116
Dan Rusu <dangingko@yahoo.com>
2025-01-14 22:48:59 +0200

    node streams practice

---
+++ src/streams.js
@@ -0,0 +1,28 @@
+const { Transform } = require('node:stream');
+const { join } = require('node:path');
+const { createReadStream, createWriteStream } = require('node:fs');
+
+// input file has 3kb
+const inputFilePath = join(__dirname, '..', 'data', 'lorem-input.txt');
+const outputFilePath = join(__dirname, '..', 'data', 'lorem-output.txt');
+
+// { highWaterMark: 1024 } read stream chunk size 1kb
+// default chunk size is 665536 bytes
+const fileInputStream = createReadStream(inputFilePath, {
+  highWaterMark: 1024,
+});
+
+const fileOutputStream = createWriteStream(outputFilePath);
+
+const toUppercaseTransformStream = new Transform({
+  transform(chunk, encoding, callback) {
+    this.push(chunk.toString().toUpperCase());
+    setTimeout(callback, 1000); // to see chunk being proccesed
+    // callback(); // chunk proccesing is finished
+  },
+});
+
+// readStream.pipe(writeStream) returns a readStream
+
+fileInputStream.pipe(toUppercaseTransformStream).pipe(process.stdout);
+// inputStream.pipe(toUppercaseTransformStream).pipe(fileOutputStream);
