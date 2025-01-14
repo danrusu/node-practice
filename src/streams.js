c4bdcbaddc5eb79f9914dbc4b51b58bc3745e470
Dan Rusu <dangingko@yahoo.com>
2025-01-14 23:33:08 +0200

    add gzip transform stream

--- src/streams.js
+++ src/streams.js
@@ -1,10 +1,12 @@
 const { Transform } = require('node:stream');
+const zlib = require('node:zlib');
 const { join } = require('node:path');
 const { createReadStream, createWriteStream } = require('node:fs');
 
 // input file has 3kb
 const inputFilePath = join(__dirname, '..', 'data', 'lorem-input.txt');
 const outputFilePath = join(__dirname, '..', 'data', 'lorem-output.txt');
+const gzipFilePath = join(__dirname, '..', 'data', 'lorem-output.gzip');
 
 // { highWaterMark: 1024 } read stream chunk size 1kb
 // default chunk size is 64 kb
@@ -12,17 +14,27 @@ const fileInputStream = createReadStream(inputFilePath, {
   highWaterMark: 1024, // 1 kb
 });
 
+// output (write) streams
 const fileOutputStream = createWriteStream(outputFilePath);
+const gzipOutputStream = createWriteStream(gzipFilePath);
 
+// transform streams
 const toUppercaseTransformStream = new Transform({
   transform(chunk, encoding, callback) {
     this.push(chunk.toString().toUpperCase());
-    setTimeout(callback, 1000); // to see chunk being proccesed
-    // callback(); // chunk proccesing is finished
+    // setTimeout(callback, 1000); // to see chunk being proccesed when output is stdout
+    callback(); // chunk proccesing is finished
   },
 });
+const gzipTransformStream = zlib.createGzip();
 
-// readStream.pipe(writeStream) returns a readStream
+// TIP: readStream.pipe(writeStream) returns a readStream
 
-fileInputStream.pipe(toUppercaseTransformStream).pipe(process.stdout);
-// inputStream.pipe(toUppercaseTransformStream).pipe(fileOutputStream);
+// fileInputStream.pipe(toUppercaseTransformStream).pipe(process.stdout);
+
+// fileInputStream.pipe(toUppercaseTransformStream).pipe(fileOutputStream);
+
+fileInputStream
+  .pipe(toUppercaseTransformStream)
+  .pipe(gzipTransformStream)
+  .pipe(gzipOutputStream);
